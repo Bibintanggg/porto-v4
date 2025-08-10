4a84e82be56ae5241bee0e0c00eb3791cc73bdc1
Bibintanggg <bintangyudhaa8@gmail.com>
2025-08-11 00:37:06 +0700

    feat: new nav with framer motion, add types and data for projects section

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,7 +1,9 @@
+import Navbar from "../components/Navbar";
+
 export default function Home() {
     return (
-        <div className="font-mono">
-            hello
+        <div className="font-mono h-[500vh]">
+            <Navbar/>
         </div>
     )
 }
---
+++ src/data/project.ts
@@ -0,0 +1,12 @@
+import { Project } from "@/types/project";
+
+export const project: Project[] = [
+    {
+        id: 1,
+        title: "",
+        description: "",
+        image: "",
+        techStack: ['', ''],
+        link: ""
+    }
+]
